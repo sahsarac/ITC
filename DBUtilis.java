aed77946166d8ed260193f62b58629884e88145c
Pavan Paturi <pvnp02@gmail.com>
2019-03-19 11:42:52 +0530

    update1 for development branch

--- DBUtilis.java
+++ DBUtilis.java
@@ -2,6 +2,6 @@ This is a bad line of code
 This is modified
 This is DBUtilis file for java Developers
 
-This is for devlopment branch to work234 work1234
+This is for devlopment branch to work234 work1234i Development Branch Development branch
 line 1 of code
 line 2 of code
